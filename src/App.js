7f53fe2de100bd76cc22f0768a912d5443de854c
Devendra <devendranathreddy54@gmail.com>
2023-10-29 17:18:11 +0530

    adding challenges and ref to application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Player from './components/Player';
+import TimerChallenge from './components/TimerChallenge';
 
 function App() {
   return (
     <>
       <div id="content">
-        <h1>The <span className='em'>Almost</span> Final Countdown</h1>
+        <h1>The <em>Almost</em> Final Countdown</h1>
         <header>
           <p> Stop the timer once you estimate that time is (almost) up</p>
         </header>
         <Player />
+
+        <div id="challenges">
+          <TimerChallenge title='Easy' targetTime={1} />
+          <TimerChallenge title='Medium' targetTime={5} />
+          <TimerChallenge title='Tough' targetTime={10} />
+          <TimerChallenge title='Pros only' targetTime={15} />
+        </div>
       </div>
-      <div id="challenges"></div>
     </>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-    const [name, setName] = useState('')
-    const [submitted, setSubmitted] = useState(false)
+    const playerName = useRef()
+    const [name, setName] = useState(null)
 
     const handleSubmit = () => {
-        setSubmitted(true)
+        setName(playerName.current.value)
+
     }
+
     return (
         <section id="player">
-            <h2>Welcome {submitted ? name : 'unknown entity'}</h2>
+            <h2>Welcome {name ? name : 'unknown entity'}</h2>
             <p>
-                <input type="text" onChange={(e) => setName(e.target.value)} />
+                <input ref={playerName} type="text" />
                 <button onClick={handleSubmit}>Set Name</button>
             </p>
         </section>
